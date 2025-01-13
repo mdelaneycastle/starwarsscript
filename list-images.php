0ddc7b50677b8eb5e9bffe31d69ca02c02dd0932
mdelaneycastle <154420982+mdelaneycastle@users.noreply.github.com>
2025-01-13 10:49:37 +0000

    Delete list-images.php

--- list-images.php
+++
@@ -1,9 +0,0 @@
-<?php
-$directory = './Images/';
-$files = array_diff(scandir($directory), array('..', '.')); // Exclude . and ..
-$imageFiles = array_filter($files, function($file) use ($directory) {
-    $ext = pathinfo($file, PATHINFO_EXTENSION);
-    return in_array(strtolower($ext), ['png', 'jpg', 'jpeg', 'gif']);
-});
-echo json_encode(array_values($imageFiles));
-?>
